4687b700c72cd0b5329b55b933a5096aa8bb3ef8
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-21 13:56:15 -0500

    move customer name params into application controller

--- app/controllers/api/customer_treasures_controller.rb
+++ app/controllers/api/customer_treasures_controller.rb
@@ -2,17 +2,8 @@ class Api::CustomerTreasuresController < ApplicationController
   respond_to :json
 
   def show
-    @customer_treasures = CustomerTreasures.for(show_params[:customer_name])
+    @customer_treasures = CustomerTreasures.for(customer_name_params[:customer_name])
 
     respond_with @customer_treasures
-  end
-
-  private
-
-  def show_params
-    params.permit(:customerName).reduce({}) { |acc, (k,v)|
-      acc[k.to_s.underscore.to_sym] = v
-      acc
-    }
   end
 end
