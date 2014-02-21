1b1bda4f0be25d090410ebd02c96da978480353a
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-21 13:53:52 -0500

    pair down Store::CustomerStore's responsibilities
    Simplify, simplify.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,34 +1,9 @@
-require_relative '../../lib/customer_data'
-
-class Store
-  attr_reader :customer_name, :customer_list, :customer_data
-  def initialize(customer_name: nil, customer_data: CustomerData, customer_list: Customer)
-    @customer_name = customer_name
-    @customer_data = customer_data
-    @customer_list = customer_list
-
-  end
-
-  def customer
-    @customer ||= customer_with_karma
-  end
-
-  private
-
-  def customer_with_karma
-    customer = lookup_customer
-    karma = customer_data.available_karma(customer_name)
-    Hashie::Mash.new(customer.attributes.merge(available_karma: karma))
-  end
-
-  def lookup_customer
-    customer_list.where(name: customer_name).first || NullCustomer.new(customer_name)
-  end
-
-  NullCustomer = Struct.new(:name) do
-    def attributes
-      Hash[id: nil,
-        name: name]
+module Store
+  class CustomerStore
+    attr_reader :customer, :products
+    def initialize(customer: nil, products: nil)
+      @customer = customer
+      @products = products
     end
   end
 end
--- spec/unit/store_spec.rb
+++ spec/unit/store_spec.rb
@@ -1,41 +1,19 @@
 require 'spec_helper'
 require_relative '../../app/models/store'
 
-describe Store do
+describe Store::CustomerStore do
   describe 'customer information' do
-    describe 'when there is a customer with a purchase history' do
-      let(:db_customer) { double('Customer', attributes: Hash[name: 'name', id: 1]) }
-      let(:customer_data) { double('CustomerData', available_karma: 100) }
-      let(:customer_list) { double('Customer', where: [db_customer]) }
-      let(:store) { Store.new(customer_name: 'name',
-          customer_data: customer_data,
-          customer_list: customer_list) }
-      let(:customer) { store.customer }
+    let(:customer) { double('Store::Customer') }
+    let(:products) { double('Store::Products') }
+    let(:store) { Store::CustomerStore.new(customer: customer,
+        products: products) }
 
-      it 'finds the customer' do
-        expect(customer.id).to eq(1)
-        expect(customer.name).to eq('name')
-      end
-
-      it 'inserts the customer karma' do
-        expect(customer.available_karma).to eq(100)
-      end
+    specify 'customer' do
+      expect(store.customer).to eq(customer)
     end
 
-    describe 'when there is no customer' do
-      let(:customer_list) { double('Customer', where: []) }
-
-      it 'returns a customer with no id, but with karma and the right name' do
-        customer_data = double('CustomerData', available_karma: 100)
-        store = Store.new(customer_name: 'name',
-          customer_list: customer_list,
-          customer_data: customer_data)
-        customer = store.customer
-
-        expect(customer.id).to be_nil
-        expect(customer.available_karma).to eq(100)
-        expect(customer.name).to eq('name')
-      end
+    specify 'products' do
+      expect(store.products).to eq(products)
     end
   end
 end
