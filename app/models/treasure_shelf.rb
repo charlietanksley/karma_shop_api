c8599d0a6e7c291757aa952035025230674fd250
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-25 12:10:49 -0500

    dead code

--- app/models/treasure_shelf.rb
+++ app/models/treasure_shelf.rb
@@ -1,6 +1,4 @@
 class TreasureShelf < ActiveRecord::Base
-  # validates :customer_id, uniqueness: true
-
   belongs_to :customer, inverse_of: :treasure_shelf
   has_many :treasures, inverse_of: :treasure_shelf
 
