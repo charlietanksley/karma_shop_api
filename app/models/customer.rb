92cc0030fc227fde14aec29d2bc611abbc9d3704
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-02-25 09:08:43 -0500

    make sure customers have treasure shelves

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
+require_relative 'treasure_shelf'
+
 class Customer < ActiveRecord::Base
+  after_create :ensure_treasure_shelf
+
   validates :name, presence: true, uniqueness: true
 
   has_one :treasure_shelf, dependent: :destroy
@@ -6,5 +10,11 @@ class Customer < ActiveRecord::Base
   def treasures
     return Array.new if treasure_shelf.nil?
     treasure_shelf.treasures
+  end
+
+  private
+
+  def ensure_treasure_shelf
+    create_treasure_shelf if treasure_shelf.nil?
   end
 end
