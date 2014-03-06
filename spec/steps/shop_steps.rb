e3f49b636793e89c5b9f94b758ea1036d1787acc
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-03-06 11:06:09 -0500

    fix class name for item details

--- spec/steps/shop_steps.rb
+++ spec/steps/shop_steps.rb
@@ -1,5 +1,5 @@
 step 'I should see the sale item :name' do |name|
-  expect(page.find('.product-details')).to have_content(name)
+  expect(page.find('.store-item-details')).to have_content(name)
 end
 
 step 'I buy :name' do |name|
