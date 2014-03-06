26f845ccbad69f5ab7026edddbf9c3baa01c8ee7
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-03-06 12:36:43 -0500

    add feature for viewing all products

--- spec/steps/navigation_steps.rb
+++ spec/steps/navigation_steps.rb
@@ -2,12 +2,17 @@ step 'I visit the main page' do
   visit root_path
 end
 
+step 'I visit the products page' do
+  visit root_path
+  click_link('All products')
+end
+
+step 'I visit the shopping page for :customer_name' do |customer_name|
+  send 'I visit the treasure page for :customer_name', customer_name
+  click_link('Go shopping')
+end
+
 step 'I visit the treasure page for :customer_name' do |customer_name|
   visit root_path
   visit "/treasures/#{customer_name}"
-end
-
-step 'I vist the shopping page for :customer_name' do |customer_name|
-  send 'I visit the treasure page for :customer_name', customer_name
-  click_link('Go shopping')
 end
--- spec/steps/product_steps.rb
+++ spec/steps/product_steps.rb
@@ -1,3 +1,7 @@
+step 'I should see the product :name' do |name|
+  send 'I should see the treasure :treasure_name', name
+end
+
 step 'the product :name exists' do |name|
   Macro.create_product(name: name)
 end
